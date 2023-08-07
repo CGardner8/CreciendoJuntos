16974665adcfe9c2ad28cde78a0d29b82a928738
Claudia Gardner <claudia.gardner21@gmail.com>
2023-08-06 21:15:30 -0700

    Agregué links y modificaciones al footer

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ Pagina web para kinder `Creciendo Juntos`.
 
 ## Todo
 
-- Cambie color al poner cursor encima en links nav
-- Agregar icono FB y maps o whatsapp a la barra nav
-- Barra nav sticky
-- Barra avisos
-- Mas abajo foto4
-- Niveles educativos en tarjetas
+* Cambie color al poner cursor encima en links nav
+* Agregar icono FB y maps o whatsapp a la barra nav
+* Barra nav sticky
+* Barra avisos
+* Mas abajo foto4
+* Niveles educativos en tarjetas
 
-* footer o pie de página con dirección y teléfono
-* Redireccionar los links de nav
-* links de fb y maps
-* Darle color o punch
+- footer o pie de página con dirección y teléfono
+- Redireccionar los links de nav
+- links de fb y maps
+- Darle color o punch
--- assets/script/main.js
+++ assets/script/main.js
@@ -4,20 +4,19 @@ const logoToShow = document.getElementById("logoToShow");
 
 window.addEventListener("scroll", function () {
   if (isElementVisible(firstNav)) {
-      logoToShow.classList.add("d-none");
-      logoToShow.classList.remove("d-block");
-      secondNav.classList.add('justify-content-center')
-      secondNav.classList.remove('justify-content-sm-around')
-    } else {
-      logoToShow.classList.add("d-block");
-      logoToShow.classList.remove("d-none");
-      secondNav.classList.add('justify-content-sm-around')
-      secondNav.classList.remove('justify-content-center')
+    logoToShow.classList.add("d-none");
+    logoToShow.classList.remove("d-block");
+    secondNav.classList.add('justify-content-center')
+    secondNav.classList.remove('justify-content-sm-around')
+  } else {
+    logoToShow.classList.add("d-block");
+    logoToShow.classList.remove("d-none");
+    secondNav.classList.add('justify-content-sm-around')
+    secondNav.classList.remove('justify-content-center')
   }
 });
 
 function isElementVisible(element) {
-    const rect = element.getBoundingClientRect();
-    return rect.top + 120 >= 0;
-  }
-  
+  const rect = element.getBoundingClientRect();
+  return rect.bottom >= 0;
+}
